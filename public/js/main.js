3d5836208120da19d71927e5aef0f9e92ccb094c
fhfhfh <fmahon@redhat.com>
2016-11-03 10:18:45 +0000

    dew temp

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
-console.log("boom")
+var url = window.location.hostname === 'localhost' ?
+    'http://localhost:8001/eidw' :
+    'http://nodeappfm-fintanm.rhcloud.com/eidw';
 
 
 
@@ -8,18 +10,17 @@ function getWeather() {
         "numReadings":"20"
     };
 
-    $.post('http://nodeappfm-fintanm.rhcloud.com/eidw', body, function(data){
+    $.post(url, body, function(data){
     	console.log('res', data);
     	// $('#eidw').text(JSON.stringify(data.res));
         // $('<table><tr><td>.....</td></tr></table>').appendTo( '#eidw' );
         var data = data.res;
-        var header = "<tr><th>time</th><th>temp</th><th>dew</th><th>wind</th><th>p</th><th>weather</th></tr>"
+        var header = "<tr><th>time</th><th>temp</th><th>wind</th><th>p</th><th>weather</th></tr>"
         var rows = '';
         for (var i = 0; i < data.length; i++) {
             rows=rows.concat('<tr>'+
                 '<td>'+data[i].time+'</td>'+
                 '<td>'+data[i].temp+'</td>'+
-                '<td>'+data[i].dew+'</td>'+
                 '<td>'+data[i].spd+'</td>'+
                 '<td>'+data[i].pressure+'</td>'+
                 '<td>'+data[i].weather+'</td>'+
