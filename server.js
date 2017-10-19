cbc4a70c864962901f85a78225d983a8b35048a2
fhfhfh <fmahon@redhat.com>
2017-10-19 15:25:43 +0100

    server update

--- server.js
+++ server.js
@@ -38,3 +38,5 @@ var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var server = app.listen(port, host, function() {
   console.log("App started at: " + new Date() + " on port: " + port); 
 });
+
+module.exports = app ;
